286528d88701b8e84054278ee003aa3b851d1d53
JeremyGu2021 <jgu@apra.com.au>
2024-02-28 20:57:24 +1100

    minor refactor

--- init_vector_store.py
+++ init_vector_store.py
@@ -1,44 +1,26 @@
 import json
 import os
-import boto3
 import numpy as np
 from pinecone import Pinecone
-
-def query_endpoint_with_json_payload(encoded_json):
-    client = boto3.client('runtime.sagemaker')
-    response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=encoded_json)
-    return response
+from sagemaker_utils import get_text_embedding, parse_text_embedding_response
 
 
-def parse_response_multiple_texts(query_response):
-    model_predictions = json.loads(query_response['Body'].read())
-    embeddings = model_predictions['embedding']
-    return embeddings
 
 # Step 1: Read text files
 folder_path = 'apra_knowledgebase'
 text_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.txt')]
 texts = []
-print('text_files', text_files)
+file_names = []  # List to keep track of filenames
 for file_path in text_files:
     with open(file_path, 'r', encoding='utf-8') as file:
         texts.append(file.read())
-
-session = boto3.Session(
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
-    region_name=os.getenv("AWS_REGION")
-)
+        file_names.append(os.path.basename(file_path))  # Save the filename
 
 endpoint_name = 'jumpstart-dft-hf-textembedding-all-minilm-l6-v2-new'
 
-sagemaker_runtime = session.client('runtime.sagemaker')
-embeddings = []
-
 payload = {"text_inputs": texts}
-query_response = query_endpoint_with_json_payload(json.dumps(payload).encode('utf-8'))
-embeddings = parse_response_multiple_texts(query_response)
+query_response = get_text_embedding(json.dumps(payload).encode('utf-8'))
+embeddings = parse_text_embedding_response(query_response)
 
 # Initialize Pinecone environment
 api_key = "19ff39f1-6252-456e-a6ca-a257faa2df8b"
@@ -49,7 +31,7 @@ index = pc.Index(host="https://langchainpinecone-of7knlv.svc.apw5-4e34-81fa.pine
 
 # Update Pinecone index with embeddings
 # Make sure you have unique IDs for each text/document  
-for i, (text, embedding) in enumerate(zip(texts, embeddings)):
+for i, ((text, file_name), embedding) in enumerate(zip(zip(texts, file_names), embeddings)):
     vector_id = str(i)  # Unique identifier for the vector
-    metadata = {"original_text": text}  # Store original text as metadata
+    metadata = {"original_text": text, "source": file_name}  # Include original text, filename in metadata
     index.upsert(vectors=[(vector_id, embedding, metadata)])
